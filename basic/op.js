bd3d481400fb12cd4aadbc2f308fbb137b3305f3
Harshit16112000 <61346369+Harshit16112000@users.noreply.github.com>
2023-11-21 15:43:59 +0000

    Overview of Javascript ds and op

---
+++ basic/op.js
@@ -0,0 +1,6 @@
+let value = 3
+let negvalue = -value
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(2 + 2 + "1")
--- basic/test.js
+++ basic/test.js
@@ -1,1 +1,49 @@
+"use strict";  // treat all JS code as newer version
 console.log("Shivay")
+
+var a = "harshit";
+console.log(a)
+
+// alert(3+3)  // Using nodejs , not browser
+ let name = "harsh"
+ let age = 3
+ let isLogedIn = false
+
+
+ // data type
+ // null -> standalone value, it is empty
+ // Example -> Rewuested a temp from client, but no response came, so, 
+ // instead of passing zero, it will pass as empty value(null)
+ //undefined -> when a value is not defined
+ // symbol => to find uniqueness
+ 
+
+ // object
+ //console.log(typeof null);  //objeect
+ //console.log(typeof undefined);  //undefined
+
+ 
+ let score = "33abv";
+ console.log(typeof score);
+ let valueInNumber = Number(score);
+ console.log(typeof score);
+ console.log(valueInNumber);  //NaN Not an Number
+
+ let test = undefined;
+ console.log(typeof test);
+ let valueInNumberfornull = Number(test);
+ console.log(typeof test);
+ console.log(valueInNumberfornull)  // 0
+
+
+ /*
+"33"  => 33
+"33abc"  => NaN
+true => 1
+null  => 0
+undefined => NaN
+ */
+
+// For Boolean   0, "" -> False 
+
+ 
